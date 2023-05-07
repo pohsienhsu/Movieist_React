46c2e7e318977107ce9b445bce148a45b174c735
phsu40 <phsu40@gatech.edu>
2023-05-07 16:08:48 -0700

    Integrate key attributes to each Paper component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 import Layout from './components/Layout';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/home/Home';
+import Header from './components/header/Header';
 
 function App() {
 
@@ -28,6 +29,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home movies={movies} />}></Route>
